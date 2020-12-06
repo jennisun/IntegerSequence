3b7611e5cc0158423bb1a0503e34096487004473
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-12-06 11:36:47 -0500

    range

---
+++ Range.java
@@ -0,0 +1,18 @@
+import java.util.NoSuchElementException;
+
+public class Range implements IntegerSequence{
+  private int start,end,current;
+
+  public Range(int start,  int end){
+    start = this.start;
+    end = this.end;
+    current = this.start;
+  }
+  public void reset(){  }
+  public int length(){  }
+  public boolean hasNext(){  }
+
+  //@throws NoSuchElementException
+  public int next(){    }
+
+}
