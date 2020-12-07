1b3495d603b45f6aed7d1f8030d999a41cfa9d67
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-12-07 14:14:50 -0500

    class 2

--- ArraySequence.java
+++ ArraySequence.java
@@ -6,10 +6,21 @@ public class ArraySequence implements IntegerSequence{
 
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int [] other){
+    data = new int[other.length];
     for (int i = 0; i < other.length; i ++) {
       data[i] = other[i];
     }
   }
+
+  public ArraySequence(IntegerSequence otherseq) {
+		data = new int[otherseq.length()];
+
+		for (int i = 0; i < otherseq.length(); i++) {
+			data[i] = otherseq.next();
+		}
+    otherseq.reset();
+	}
+
 
   public void reset(){
     currentIndex = 0;
@@ -20,7 +31,7 @@ public class ArraySequence implements IntegerSequence{
   }
 
   public boolean hasNext(){
-    return currentIndex != data.length;
+    return currentIndex != data.length - 1;
   }
 
   //@throws NoSuchElementException
