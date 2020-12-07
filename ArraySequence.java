c7e1d5c903f74dd7aca88588c92a20b472006ced
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-12-07 13:36:49 -0500

    class

---
+++ ArraySequence.java
@@ -0,0 +1,12 @@
+import java.util.NoSuchElementException;
+public class ArraySequence implements IntegerSequence{
+  int currentIndex;
+  int []data;
+
+  /*Construct the sequence by copying values from the other array into the data array*/
+  public ArraySequence(int [] other){
+    for (int i = 0; i < other.length; i ++) {
+      data[i] = other[i];
+    }
+  }
+}
